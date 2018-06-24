94bea8765af3d32b289ac57a2c6831e762ccdb09
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-06-24 15:06:47 +0300

    ImplementingOfMVC

---
+++ KechingramMessenger/Login/LoginView/LoginView.swift
@@ -0,0 +1,15 @@
+//
+//  LoginView.swift
+//  KechingramMessenger
+//
+//  Created by Nikita Kechinov on 24.06.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    
+
+}
---
+++ KechingramMessenger/Login/LoginViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  KechingramMessenger
+//
+//  Created by Nikita Kechinov on 24.06.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
