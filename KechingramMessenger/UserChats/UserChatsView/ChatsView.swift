8ab1ac724758a460f1b4d95cfb2c6e62a8e18772
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-07-23 12:43:31 +0300

    Testing updated ChatView animation

---
+++ KechingramMessenger/UserChats/UserChatsView/ChatsView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatsView.swift
+//  KechingramMessenger
+//
+//  Created by Nikita Kechinov on 23.07.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import UIKit
+
+class ChatsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
